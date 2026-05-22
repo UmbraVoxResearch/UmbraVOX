34ed2eec082d5bf93f85c96d812b789b8bfe146c
cyanitol <gitnoreply@cyanitol.com>
2026-05-22 06:34:48 -0400

    codegen: regenerate 8 new C + FFI files from fixed codegen tool

--- src/UmbraVox/Crypto/Generated/FFI/NetworkProtocol.hs
+++ src/UmbraVox/Crypto/Generated/FFI/NetworkProtocol.hs
@@ -1,26 +1,13 @@
 -- SPDX-License-Identifier: Apache-2.0
--- | Auto-generated FFI bindings by CryptoGen. DO NOT EDIT.
+-- | Auto-generated FFI bridge bindings by CryptoGen. DO NOT EDIT.
 {-# LANGUAGE ForeignFunctionInterface #-}
-module UmbraVox.Crypto.Generated.FFI.NetworkProtocol where
+module UmbraVox.Crypto.Generated.FFI.NetworkProtocol
+    ( ffiLinked
+    ) where
 
-import Data.Word (Word8, Word32, Word64)
 import Foreign.C.Types (CInt(..))
-import Foreign.Ptr (Ptr)
-import Data.ByteString (ByteString)
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Unsafe as BSU
 
-foreign import ccall "networkprotocol" c_networkprotocol :: Ptr Word8 -> Ptr Word8 -> Ptr Word8 -> Ptr Word8 -> Ptr Word8 -> Ptr Word8 -> Ptr Word8 -> Ptr Word8 -> Ptr Word8 -> IO Word32
+foreign import ccall "networkprotocol_link_probe" c_networkprotocol_link_probe :: IO CInt
 
-networkprotocol :: ByteString -> ByteString -> ByteString -> ByteString -> ByteString -> ByteString -> ByteString -> ByteString -> ByteString -> IO Word32
-networkprotocol msg_type hs_version hs_public_key hs_capabilities data_sequence data_payload ack_sequence peer_list wire_bytes =
-  BSU.unsafeUseAsCStringLen msg_type $ \(msg_type_ptr, _) ->
-  BSU.unsafeUseAsCStringLen hs_version $ \(hs_version_ptr, _) ->
-  BSU.unsafeUseAsCStringLen hs_public_key $ \(hs_public_key_ptr, _) ->
-  BSU.unsafeUseAsCStringLen hs_capabilities $ \(hs_capabilities_ptr, _) ->
-  BSU.unsafeUseAsCStringLen data_sequence $ \(data_sequence_ptr, _) ->
-  BSU.unsafeUseAsCStringLen data_payload $ \(data_payload_ptr, _) ->
-  BSU.unsafeUseAsCStringLen ack_sequence $ \(ack_sequence_ptr, _) ->
-  BSU.unsafeUseAsCStringLen peer_list $ \(peer_list_ptr, _) ->
-  BSU.unsafeUseAsCStringLen wire_bytes $ \(wire_bytes_ptr, _) ->
-  c_networkprotocol msg_type_ptr hs_version_ptr hs_public_key_ptr hs_capabilities_ptr data_sequence_ptr data_payload_ptr ack_sequence_ptr peer_list_ptr wire_bytes_ptr
+ffiLinked :: IO Bool
+ffiLinked = (/= 0) <$> c_networkprotocol_link_probe
