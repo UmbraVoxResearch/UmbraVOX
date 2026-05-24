44f3e1940ccd723bcc9706254648ade91f72ed06
cyanitol <gitnoreply@cyanitol.com>
2026-05-24 14:19:06 -0400

    doc: CHANGELOG v0.5.16 + version sync + TODO project status summary

--- src/UmbraVox/Version.hs
+++ src/UmbraVox/Version.hs
@@ -12,7 +12,7 @@ module UmbraVox.Version
 -- | Version string, e.g. "v0.4.0".
 -- Kept in sync with the @version@ field in @UmbraVox.cabal@.
 version :: String
-version = "v0.5.15"
+version = "v0.5.16"
 
 -- | Full version with app name, e.g. "UmbraVOX v0.4.0"
 versionFull :: String
