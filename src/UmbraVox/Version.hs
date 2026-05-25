9511ce2bb57b55df725afe960ac5e29536307d99
cyanitol <gitnoreply@cyanitol.com>
2026-05-25 10:36:50 -0400

    v0.5.20: Signal Server fat JAR (333MB), Firecracker TUI verified

--- src/UmbraVox/Version.hs
+++ src/UmbraVox/Version.hs
@@ -12,7 +12,7 @@ module UmbraVox.Version
 -- | Version string, e.g. "v0.4.0".
 -- Kept in sync with the @version@ field in @UmbraVox.cabal@.
 version :: String
-version = "v0.5.19"
+version = "v0.5.20"
 
 -- | Full version with app name, e.g. "UmbraVOX v0.4.0"
 versionFull :: String
